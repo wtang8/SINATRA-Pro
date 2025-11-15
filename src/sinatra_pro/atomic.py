809f440f7594c53c123f1214899199b6566b6894
wtang8 <wstangp@gmail.com>
2025-11-15 23:33:03 +0000

    center atoms

--- src/sinatra_pro/atomic.py
+++ src/sinatra_pro/atomic.py
@@ -276,6 +276,11 @@ def convert_pdb_to_meshes(
         if align_sequence and seqsel_B is not None:
             _align_selection = "resid " + " ".join(str(r.resid) for r in seqsel_B) + " and " + align_selection
         atoms_mesh = u.select_atoms(mesh_selection)
+        # center atoms to atoms_align
+        center_of_mass = atoms_mesh.center_of_mass()
+        atoms_mesh.translate(-center_of_mass)
+        atoms_align.translate(-center_of_mass)
+        
         if len(atoms_mesh) == 0:
             raise ValueError(
                 f"Mesh selection '{mesh_selection}' returned no atoms in {pdb_file}"
